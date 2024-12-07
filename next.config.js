e5b356b8da1695f7b1dffb44d8259c72fb193197
issadeen <issadeenabdiali@outlook.com>
2024-12-07 10:01:56 +0300

    nex.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-import { NextConfig } from "next";
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+module.exports = nextConfig;
