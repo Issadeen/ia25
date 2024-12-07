d647ffa82e3ba1ae08a239caea7ed95457734692
issadeen <issadeenabdiali@outlook.com>
2024-12-07 19:00:48 +0300

    tr5

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
   images: {
-    unoptimized: false
-  }
+    unoptimized: false,
+  },
 };
 
 export default nextConfig;
