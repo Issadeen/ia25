9a183879a416b2e2b9a3d98d2c73a8d1635e4238
issadeen <issadeenabdiali@outlook.com>
2024-12-07 20:34:18 +0300

    ok

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   images: {
-    unoptimized: false,
+    unoptimized: true,
   },
+  basePath: '/your-repo-name',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
