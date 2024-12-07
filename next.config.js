f7145509c17e87f51b026af80e6f81870d019c44
issadeen <issadeenabdiali@outlook.com>
2024-12-07 13:33:58 +0300

    v0

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true
-  },
-  images: {
-    unoptimized: true
-  }
+  reactStrictMode: true,
+  // Images will now be optimized by Next.js
 };
 
 export default nextConfig;
