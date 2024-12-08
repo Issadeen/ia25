d1ca1d91af2e9eeb52c4560cd27e9bcb776c6aa0
issadeen <issadeenabdiali@outlook.com>
2024-12-08 20:16:25 +0300

    tr43

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,7 +2,11 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { ThemeProviderProps } from "next-themes"
+
+interface ThemeProviderProps {
+  children: React.ReactNode
+  // Add other props if necessary
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
